e1019264fe95b34b16bbe0a6226e20a26df7ff91
Amee Parmar <amee05@yahoo.com>
2020-11-19 22:12:05 -0800

    getting there with some bugs

--- index.js
+++ index.js
@@ -1,35 +1,89 @@
+const inquirer = require("inquirer");
 const fs = require('fs');
-const inquirer = require('inquirer');
-const axios = require('axios');
-
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-  }
-
-
-])
-
-
-
-
-
-
-
-
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?"
+  },
+  {
+    type: "input",
+    name: "badge",
+    message: "Please provide the badges links that you want"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide your project's description"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide the installation instructions"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide the project usage"
+  },
+  {
+    type: "input",
+    name: "licence",
+    message: "Please provide the project licence or your badge link"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please provide the contributing parties"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide the project tests"
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github user name?"
+  },
+  {
+    type: "input",
+    name: "repo",
+    message: "What is your repo link?"
+  },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer
+  .prompt(questions)
+  .then(function (data) {
+    const queryUrl = `https://api.github.com/users/${data.username}`;
 
-// function to initialize program
+    axios.get(queryUrl).then(function (res) {
+
+      const githubInfo = {
+        githubImage: res.data.avatar_url,
+        email: res.data.email,
+        profile: res.data.html_url,
+        name: res.data.name
+      };
+      
+      // function to write README file
+      fs.writeFile("README.md", generate(data, githubInfo), function (err) {
+        if (err) {
+          throw err;
+        };
+
+        console.log("New README file created with success!");
+      });
+    });
+
+  });
+
+  // function to initialize program
 function init() {
 
 }
